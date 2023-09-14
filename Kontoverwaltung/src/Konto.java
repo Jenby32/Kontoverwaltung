321437d28ca82a0d877bb66eab991f6d062b39d0
Jenby32 <kevinkozar2005@gmail.com>
2023-09-14 11:02:59 +0200

    Adding Account classes.

---
+++ Kontoverwaltung/src/Konto.java
@@ -0,0 +1,12 @@
+public class Konto {
+    private String accOwner;        // Kontoinhaber
+    private String blz;             // Bankleitzahl
+    private String accNr;           // Kontonummer
+    private String overdraftLimit;  // Konto Überziehungsrahmen
+    private float fees;             // Kontofuehrungsgebuehren
+    private float balance;          // Kontostand
+
+    Konto() {
+        
+    }
+}
---
+++ Kontoverwaltung/src/Kreditkonto.java
@@ -0,0 +1,11 @@
+public class Kreditkonto {
+    private String accOwner;        // Kontoinhaber
+    private String blz;             // Bankleitzahl
+    private String accNr;           // Kontonummer
+    private String overdraftLimit;  // Konto Überziehungsrahmen
+    private float fees;             // Kontofuehrungsgebuehren
+    private float balance;          // Kontostand
+    Kreditkonto() {
+
+    }
+}
