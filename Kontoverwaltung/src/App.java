699c7ee2455e217295237077c649d25da910b0ac
Jenby32 <kevinkozar2005@gmail.com>
2023-09-14 15:32:11 +0200

    last commit before first push

--- Kontoverwaltung/src/App.java
+++ Kontoverwaltung/src/App.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-
+        List<Konto> kontos = new ArrayList<Konto>(10);
         // Testcases fuer Girokonto
         System.out.println("Girokonto!!!!!");
         // erstellen des girokontos
         Girokonto gk1 = new Girokonto("Kevin Kozar", "12456", "1234 1234 1234 1234", -5000, 50, 3400);
+        kontos.add(gk1);
         System.out.println("---------------------------------Kontoauszug 1------------------------------------");
         gk1.bankStatement();
         System.out.println("---------------------------------Kontoauszug 2-------------------------------");
@@ -21,6 +25,7 @@ public class App {
         System.out.println("Kreditkonto!");
         // erstellen des Kreditkontos
         Kreditkonto kk1 = new Kreditkonto("XXXX XXXX", "12456", "1234 1234 12sdff34 1234", 0, 50, -100000);
+        kontos.add(kk1);
         System.out.println("---------------------------------Kontoauszug 1-------------------------------");
         kk1.bankStatement();
         System.out.println("---------------------------------Kontoauszug 2-------------------------------");
@@ -34,6 +39,7 @@ public class App {
         System.out.println("Sparkonto!");
         // erstellen des sparkontos
         Sparkonto sk1 = new Sparkonto("XXXX XXXX", "12456", "1234 1234 12sdff34 1234", 0, 70, 145000);
+        kontos.add(sk1);
         System.out.println("---------------------------------Kontoauszug 1-------------------------------");
         sk1.bankStatement();
         System.out.println("---------------------------------Kontoauszug 2-------------------------------");
@@ -45,5 +51,10 @@ public class App {
         System.out.println("---------------------------------Kontoauszug 4-------------------------------");
         sk1.remBalance(150000);
         sk1.bankStatement();
+
+
+        for(Konto konto : kontos) {
+            System.out.println(konto);
+        }
     }
 }
